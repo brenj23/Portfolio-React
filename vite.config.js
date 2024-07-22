33a622ab93616d194b28b5c4bb183788a3333b11
Bren Broussard <broussard.j.bren@gmail.com>
2024-07-22 12:54:55 -0500

    Install vite-plugin-env-compatible and update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), envCompatible()],
   server: {
     port: process.env.PORT || 3000,  // Use the PORT environment variable if available
     open: true,
